36afb456c2fcc40240348e75161d1b2b637218c0
MariaChaptykova1511 <masha.chaptykova@mail.ru>
2021-03-24 21:49:41 +0700

    запроссы

--- script.js
+++ script.js
@@ -20,24 +20,25 @@
       const webSaitUser=html_url;
       console.log(photo,nameUser, infoUser, webSaitUser);
 
-
       let userName=document.querySelector('.userName').innerHTML=`Пользователь: `+(name);
       if (name != null) {
          } else {
          name.innerHTML = 'Данные отсутствуют';
-      }
-      name.addEventListener("click", () => window.location = html_url);
+          }
+
       document.querySelector('.infoProfile').innerHTML=`Информация о пользователе: `+(bio);
       if (bio != null) {
          } else {
          bio.innerHTML = 'Данные отсутствуют';
-      }
-      document.querySelector('.infoweb').innerHTML=JSON.stringify(html_url);
+        }
+      let address=document.querySelector('.infoweb').innerHTML=html_url;
+      name.onclick=()=>window.location = address;
+
       const img = new Image();
       img.src = photo;
       document.body.append(img);
       if (avatar_url != null) {
          } else {
          avatar_url.innerHTML = 'Данные отсутствуют';
-      }
-        })
+        }
+    })
